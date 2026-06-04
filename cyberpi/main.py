82ccdd54c7c44f6dfb23d542f1c3e7dbe5eff8e2
Felipe dos Santos Fernandes <fsf.backend@gmail.com>
2026-06-04 15:32:38 -0300

    Update main.py
    led behavior refactor

--- cyberpi/main.py
+++ cyberpi/main.py
@@ -10,6 +10,8 @@ SERVER_PORT = 8080
 
 URL = "http://" + SERVER_IP + ":" + str(SERVER_PORT) + "/status"
 
+mode = 0  # 0 = dashboard, 1 = graph
+
 
 def show_message(msg):
     cyberpi.display.clear()
@@ -17,7 +19,6 @@ def show_message(msg):
 
 
 def connect_wifi():
-
     dots = ["   ", ".  ", ".. ", "..."]
 
     show_message("Connecting to\n" + str(WIFI_SSID))
@@ -49,7 +50,6 @@ def connect_wifi():
 
 
 def fetch_status():
-
     response = None
 
     try:
@@ -86,49 +86,93 @@ def pulse_color(base_color, intensity):
 
 
 def metric_color(value, warn, critical):
-
     if value >= critical:
         return (255, 0, 0)
-
     if value >= warn:
         return (255, 255, 0)
-
     return (0, 255, 0)
 
 
 def update_leds(cpu, ram, temp, cpu_d, ram_d, temp_d):
-
     base = metric_color(cpu, 70, 90)
     intensity = min(100, abs(cpu_d) * 12)
     r, g, b = pulse_color(base, intensity)
+    # LED principal sempre ativo
     set_led(1, r, g, b)
-    set_led(2, r, g, b)
+    # LED de alerta
+    if cpu < 80:
+        set_led(1, r, g, b)
+        set_led(2, 0, 0, 0)
+    else:
+        p = (cpu - 80) / 20.0
+        set_led(1, r, g, b)
+        set_led(
+            2,
+            int(r * p),
+            int(g * p),
+            int(b * p)
+        )
 
     base = metric_color(ram, 60, 85)
     intensity = min(100, abs(ram_d) * 12)
     r, g, b = pulse_color(base, intensity)
+    # LED principal sempre ativo
     set_led(3, r, g, b)
-    set_led(4, r, g, b)
+    # LED de alerta progressivo
+    if ram < 75:
+        set_led(4, 0, 0, 0)
+    else:
+        p = (ram - 75) / 25.0
+        set_led(
+            4,
+            int(r * p),
+            int(g * p),
+            int(b * p)
+        )
 
     base = metric_color(temp, 60, 75)
     intensity = min(100, abs(temp_d) * 10)
     r, g, b = pulse_color(base, intensity)
-    set_led(5, r, g, b)
+    if temp < 60:
+        # normal
+        set_led(5, r, g, b)
+    elif temp < 75:
+        # atenção
+        set_led(5, r, g, b)
+    else:
+        # crítico
+        # pulse = int(time.time() * 4) % 2
+        # if pulse:
+            set_led(5, 255, 0, 0)
+        # else:
+        #     set_led(5, 0, 0, 0)
 
 
-def render(host, cpu, ram, temp, battery):
-
+def render(host, cpu, ram, temp, battery, clock):
     msg = (
         "     {}\n\n"
-        " CPU       {:>3}%\n"
-        " MEM       {:>3}%\n"
-        " TEMP      {:>3}C\n"
-        " BATT      {:>3}%"
-    ).format(host, int(cpu), int(ram), int(temp), battery)
+        " CPU  (g)  {:>3}%\n"
+        " MEM  (b)  {:>3}%\n"
+        " TEMP (r)  {:>3}C\n"
+        " BATT (y)  {:>3}%"
+        "       {}\n"
+    ).format(host, int(cpu), int(ram), int(temp), battery, clock)
 
     cyberpi.display.show_label(msg, 0, 0)
 
-    update_leds(cpu, ram, temp, 0, 0, 0)
+
+def draw_graph(cpu, ram, temp, battery):
+    cyberpi.display.set_brush(80, 255, 80) # green
+    cyberpi.linechart.add(cpu)
+
+    cyberpi.display.set_brush(80, 120, 255) # blue
+    cyberpi.linechart.add(ram)
+    
+    cyberpi.display.set_brush(255, 80, 80) # red
+    cyberpi.linechart.add(temp)
+
+    cyberpi.display.set_brush(255, 255, 80) # yellow
+    cyberpi.linechart.add(battery)
 
 
 def offline_blink():
@@ -143,7 +187,7 @@ def offline_blink():
 
 
 def boot_animation():
-
+    #cyberpi.led.set_bri(100)
     colors = [
         (255, 0, 0),
         (255, 128, 0),
@@ -167,29 +211,23 @@ def boot_animation():
         r, g, b = colors[led - 1]
         cyberpi.led.on(r, g, b, led)
         time.sleep(0.15)
-
     time.sleep(0.3)
 
     for led in range(5, 0, -1):
         cyberpi.led.on(0, 0, 0, led)
         time.sleep(0.10)
-
     time.sleep(0.4)
 
     # v2
 #     for led in range(1, 6):
-
 #         r, g, b = colors[led - 1]
-
 #         cyberpi.led.on(
 #             r,
 #             g,
 #             b,
 #             led
 #         )
-
 #         time.sleep(1)
-
 #     time.sleep(1)
 
     # v1
@@ -198,11 +236,11 @@ def boot_animation():
 #         cyberpi.led.on(r, g, b, led)
 #         time.sleep(0.50)
 #         cyberpi.led.on(0, 0, 0, led)
-
 #     time.sleep(1)
 
 
 def main():
+    global mode
 
     boot_animation()
 
@@ -212,7 +250,7 @@ def main():
 
     show_message("Connected to " + str(WIFI_SSID))
 
-    time.sleep(1)
+    time.sleep(0.5)
 
     last_data = None
     last_fetch = 0
@@ -224,6 +262,11 @@ def main():
     while True:
 
         now = time.time()
+
+        if cyberpi.controller.is_press("a"):
+            mode = 1 - mode
+            cyberpi.display.clear()
+            time.sleep(0.3)
 
         if now - last_fetch > 3:
             last_fetch = now
@@ -238,12 +281,17 @@ def main():
             ram = int(last_data.get("ram", 0) or 0)
             temp = int(last_data.get("temp", 0) or 0)
             battery = int(cyberpi.get_battery() or 0)
+            clock = str(last_data.get("time", "--:--"))
 
             cpu_d = cpu - last_cpu
             ram_d = ram - last_ram
             temp_d = temp - last_temp
 
-            render(host, cpu, ram, temp, battery)
+            if mode == 0:
+                render(host, cpu, ram, temp, battery, clock)
+            else:
+                draw_graph(cpu, ram, temp, battery)
+
             update_leds(cpu, ram, temp, cpu_d, ram_d, temp_d)
 
             last_cpu = cpu
@@ -252,7 +300,7 @@ def main():
 
         else:
             offline_blink()
-            show_message("HOST\nOFFLINE\nCHECK WIFI")
+            show_message("Connection lost")
 
         time.sleep(0.5)
 
